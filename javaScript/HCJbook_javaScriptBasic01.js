88462110d3f6ef74df231b83f6b50a51e4ac8d65
vitamin777 <rlaalsdn8@gmail.com>
2022-02-22 20:01:27 +0900

    webSocket contents finish

--- javaScript/HCJbook_javaScriptBasic01.js
+++ javaScript/HCJbook_javaScriptBasic01.js
@@ -1,1 +1,7 @@
 document.write("이 글은 외부 자바스크립트 파일에서 불러온 내용입니다."+`<br>`+`<br>`);
+
+function setClock() {
+  let now = new Date()
+  document.getElementById(`clock`).innerHTML = now
+  setTimeout(`setClock()`,1000)
+}
